cad9e500d214352f608c22a85967bec10274b2d0
Mmx <1624045573@qq.com>
2021-09-16 18:27:53 +0800

    init:移入功能

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sukifans/tg
+
+go 1.16
+
+require github.com/zelenin/go-tdlib v0.4.1
---
+++ telegram.go
@@ -0,0 +1,79 @@
+package telegram
+
+import (
+	"github.com/zelenin/go-tdlib/client"
+	"log"
+	"path/filepath"
+)
+
+var c *client.Client
+
+func Init(apiId int32, apiHash string) {
+	authorizer := client.ClientAuthorizer()
+	go client.CliInteractor(authorizer)
+	authorizer.TdlibParameters <- &client.TdlibParameters{
+		UseTestDc:              false,
+		DatabaseDirectory:      filepath.Join(".tdlib", "database"),
+		FilesDirectory:         filepath.Join(".tdlib", "files"),
+		UseFileDatabase:        true,
+		UseChatInfoDatabase:    true,
+		UseMessageDatabase:     true,
+		UseSecretChats:         false,
+		ApiId:                  apiId,
+		ApiHash:                apiHash,
+		SystemLanguageCode:     "en",
+		DeviceModel:            "Server",
+		SystemVersion:          "1.0.0",
+		ApplicationVersion:     "1.0.0",
+		EnableStorageOptimizer: true,
+		IgnoreFileNames:        false,
+	}
+	var err error
+	c, err = client.NewClient(authorizer, client.WithLogVerbosity(&client.SetLogVerbosityLevelRequest{
+		NewVerbosityLevel: 0,
+	}))
+	if err != nil {
+		log.Fatalf("NewClient error: %s", err)
+	}
+}
+
+func GetHistory(chatId int64, FromMsgId int64, limit int32) (*client.Messages, error) {
+	return c.GetChatHistory(&client.GetChatHistoryRequest{
+		ChatId:        chatId,
+		FromMessageId: FromMsgId,
+		Limit:         limit,
+	})
+}
+
+func GetMsg(chatId int64, id int64) (*client.Message, error) {
+	return c.GetMessage(&client.GetMessageRequest{
+		ChatId:    chatId,
+		MessageId: id,
+	})
+}
+
+func SplitVideoMessage(msg *client.Message) (text string, fileId int32, ok bool) {
+	var t *client.MessageVideo
+	t, ok = msg.Content.(*client.MessageVideo)
+	if !ok {
+		return
+	}
+
+	text = t.Caption.Text
+	fileId = t.Video.Video.Id
+
+	return
+}
+
+func DownloadFile(fileId int32) (*client.File, error) {
+	return c.DownloadFile(&client.DownloadFileRequest{
+		FileId:   fileId,
+		Priority: 32,
+	})
+}
+
+func DownloadStat(fileId int32) (*client.File, error) {
+	return c.GetFile(&client.GetFileRequest{
+		FileId: fileId,
+	})
+}
