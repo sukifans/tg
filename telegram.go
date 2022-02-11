bb4cc7f3f0ca3830336723cc912f997a2f813b2d
Mmx233 <admin@mmxblog.com>
2022-02-11 08:16:52 +0800

    fix: 使下载文件同步为可选

--- telegram.go
+++ telegram.go
@@ -71,11 +71,11 @@ func SplitVideoMessage(msg *client.Message) (text string, fileId int32, ok bool)
 	return
 }
 
-func DownloadFile(fileId int32) (*client.File, error) {
+func DownloadFile(fileId int32, sync bool) (*client.File, error) {
 	return c.DownloadFile(&client.DownloadFileRequest{
 		FileId:      fileId,
 		Priority:    32,
-		Synchronous: true,
+		Synchronous: sync,
 	})
 }
 
